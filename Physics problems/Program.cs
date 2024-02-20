8268f2b5f6fcce0b7cb8b2ec46dcba31a81c8eb4
Jjav2 <Jjav2@HPCOMPUTER18>
2024-02-19 19:14:24 -0600

    Agregar archivos de proyecto.

---
+++ Physics problems/Program.cs
@@ -0,0 +1,14 @@
+﻿
+double gravity = 7.69;
+
+solution freefrall = new solution(gravity);
+
+// Here you take the time of fall in seconds
+Console.Write("Enter drop time in seconds: ");
+double time = Convert.ToDouble(Console.ReadLine());
+
+// Here is the operation of calculating the final operation
+double finalspeed = freefrall.finaldistance(time);
+Console.WriteLine();
+Console.WriteLine("The final velocity of the object in free fall is :"+ finalspeed); //Result
+Console.ReadLine();
---
+++ Physics problems/solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class solution
+{
+    private double _gravity;
+
+    public solution(double gravity)
+    {
+        _gravity = gravity;
+    }
+
+    public double finaldistance(double time)
+    {
+        return _gravity * time;
+    }
+}
+
